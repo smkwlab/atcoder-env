fbf239877399fcb34895f279a8a3f8279ce226e9
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2023-11-26 19:32:02 +0900

    add open file feature

--- lib/.support/bash_functions.sh
+++ lib/.support/bash_functions.sh
@@ -1,20 +1,22 @@
 function abc() {
     case $# in
         0)
-            echo "Change contest/question directory"
+            echo "Change contest/question directory and open file"
             echo ""
             echo "Usage:"
-            echo "       ${FUNCNAME[0]} <contestname> [question]"
+            echo "       ${FUNCNAME[0]} <contestname> [question [file]]"
             echo ""
             echo "Example:"
             echo "       ${FUNCNAME[0]} 123 a"
+            echo ""
+            echo "       ${FUNCNAME[0]} 123 a Main.java"
             echo ""
             return
             ;;
         1)
             target=${CONTEST_DIR}/${FUNCNAME[0]}$1
             ;;
-        2)
+        *)
             target=${CONTEST_DIR}/${FUNCNAME[0]}$1/$2
             ;;
     esac
@@ -24,26 +26,31 @@ function abc() {
     else
         echo ${target} not exists
     fi
+
+    if [ $# -eq 3 ] && [ -f $3 ]; then
+       code -r $3
+    fi
 }
 
-
-function ahc() {
+function arc() {
     case $# in
         0)
-            echo "Change contest/question directory"
+            echo "Change contest/question directory and open file"
             echo ""
             echo "Usage:"
-            echo "       ${FUNCNAME[0]} <contestname> [question]"
+            echo "       ${FUNCNAME[0]} <contestname> [question [file]]"
             echo ""
             echo "Example:"
             echo "       ${FUNCNAME[0]} 123 a"
+            echo ""
+            echo "       ${FUNCNAME[0]} 123 a Main.java"
             echo ""
             return
             ;;
         1)
             target=${CONTEST_DIR}/${FUNCNAME[0]}$1
             ;;
-        2)
+        *)
             target=${CONTEST_DIR}/${FUNCNAME[0]}$1/$2
             ;;
     esac
@@ -52,5 +59,43 @@ function ahc() {
         cd ${target}
     else
         echo ${target} not exists
+    fi
+
+    if [ $# -eq 3 ] && [ -f $3 ]; then
+       code -r $3
+    fi
+}
+
+function ahc() {
+    case $# in
+        0)
+            echo "Change contest/question directory and open file"
+            echo ""
+            echo "Usage:"
+            echo "       ${FUNCNAME[0]} <contestname> [question [file]]"
+            echo ""
+            echo "Example:"
+            echo "       ${FUNCNAME[0]} 123 a"
+            echo ""
+            echo "       ${FUNCNAME[0]} 123 a Main.java"
+            echo ""
+            return
+            ;;
+        1)
+            target=${CONTEST_DIR}/${FUNCNAME[0]}$1
+            ;;
+        *)
+            target=${CONTEST_DIR}/${FUNCNAME[0]}$1/$2
+            ;;
+    esac
+
+    if [ -d ${target} ]; then
+        cd ${target}
+    else
+        echo ${target} not exists
+    fi
+
+    if [ $# -eq 3 ] && [ -f $3 ]; then
+       code -r $3
     fi
 }
